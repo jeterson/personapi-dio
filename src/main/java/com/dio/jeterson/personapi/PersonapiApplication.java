0dd083eaa2c2f78af137e7ed881f6b3e0df02280
Jeterson Miranda Gomes <jeterson.gomes@mv.com.br>
2021-07-16 12:03:43 -0300

    First commit

---
+++ src/main/java/com/dio/jeterson/personapi/PersonapiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.jeterson.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
